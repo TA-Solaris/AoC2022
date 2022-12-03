7d43a2f99f10393ab4bca733379538fa2d3ac22f
TA-Solaris <pottered2@gmail.com>
2022-12-03 05:27:51 +0000

    Day 3

---
+++ d3/d3p1.hs
@@ -0,0 +1,29 @@
+
+import System.IO  
+import Control.Monad
+
+import Data.List
+import Data.Char (ord)
+
+translateInput :: String -> [Char]
+translateInput [] = []
+translateInput str = [helper $ splitAt (length ls `div` 2) ls | ls <- (lines str)]
+    where
+        helper :: (String, String) -> Char
+        helper (a,b) = (intersect a b) !! 0
+
+getValue :: Char -> Int
+getValue x | x `elem` ['A' .. 'Z'] = ord x - ord 'A' + 27
+           | x `elem` ['a' .. 'z'] = ord x - ord 'a' + 1
+           | otherwise = error "Invalid Character in Input [Error 1]"
+
+getSum :: [Char] -> Int
+getSum [] = 0
+getSum (x:xs) = getValue x + getSum xs
+
+main = do  
+        let list = []
+        handle <- openFile "d3/d3.txt" ReadMode
+        contents <- hGetContents handle
+        print $ getSum $ translateInput contents
+        hClose handle
---
+++ d3/d3p2.hs
@@ -0,0 +1,31 @@
+
+import System.IO  
+import Control.Monad
+
+import Data.List
+import Data.Char (ord)
+
+translateInput :: String -> [Char]
+translateInput [] = []
+translateInput str = helper $ lines str
+    where
+        helper :: [String] -> [Char]
+        helper [] = []
+        helper (a:b:c:xs) = (intersect (intersect a b) c !! 0) : (helper xs)
+        helper _ = error "Invalid Input Number of Lines [Error 2]"
+
+getValue :: Char -> Int
+getValue x | x `elem` ['A' .. 'Z'] = ord x - ord 'A' + 27
+           | x `elem` ['a' .. 'z'] = ord x - ord 'a' + 1
+           | otherwise = error "Invalid Character in Input [Error 1]"
+
+getSum :: [Char] -> Int
+getSum [] = 0
+getSum (x:xs) = getValue x + getSum xs
+
+main = do  
+        let list = []
+        handle <- openFile "d3/d3.txt" ReadMode
+        contents <- hGetContents handle
+        print $ getSum $ translateInput contents
+        hClose handle
