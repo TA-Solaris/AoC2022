00c4847490e0e54979f4c5777e81d97c380cd5aa
TA-Solaris <pottered2@gmail.com>
2022-12-06 11:05:22 +0000

    Made day 6 better

--- d6/d6p2a.hs
+++ d6/d6p2a.hs
@@ -5,10 +5,10 @@ import Control.Monad
 import Data.Set (toList, fromList)
 
 -- Chaotic evil coding
-doTaTing (x:xs) n = case (14 == (length $ toList $ fromList $ take 14 (x:xs))) of {True -> n; False -> doTaTing xs (n+1)}
+doTaTing (x:xs) n = case (14 == (length $ toList $ fromList $ take 14 (x:xs))) of {True -> n + 14; False -> doTaTing xs (n+1)}
 
 main = do
         handle <- openFile "d6/d6.txt" ReadMode
         contents <- hGetContents handle
-        print $ doTaTing contents 14
+        print $ doTaTing contents 0
         hClose handle
