6f94242f16c2646f1fea10d8d531647821e3b9a0
TA-Solaris <pottered2@gmail.com>
2022-12-08 23:51:36 +0000

    Day 8 - Removed redundant lines from solution :)

---
+++ d8/d8p2a.hs
@@ -0,0 +1,8 @@
+import System.IO  
+import Control.Monad
+import Data.List (transpose)
+main = do
+        handle <- openFile "d8/d8.txt" ReadMode
+        contents <- hGetContents handle
+        print $ maximum [foldr (*) 1 ((\x' y' grid' -> map ((let scan tree xs = if xs == [] then 0 else (if ((take 1 xs) !! 0) >= tree then 1 else 1 + scan tree (drop 1 xs)) in scan) ((grid' !! y') !! x')) $ ((\(l, r) -> [(reverse l), (drop 1 r)]) (splitAt x' (grid' !! y'))) ++ ((\(l, r) -> [(reverse l), (drop 1 r)]) (splitAt y' ((transpose grid') !! x')))) x y [[(read (x:""))::Int | x <- line] | line <- lines contents]) | x <- [0 .. length ([[(read (x:""))::Int | x <- line] | line <- lines contents] !! 0) - 1], y <- [0 .. length [[(read (x:""))::Int | x <- line] | line <- lines contents] - 1]]
+        hClose handle
