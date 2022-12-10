935be68af65423ebf9298e919f2db142bc97d655
TA-Solaris <pottered2@gmail.com>
2022-12-10 12:26:00 +0000

    Day 10

---
+++ d10/d10p1.hs
@@ -0,0 +1,47 @@
+
+import System.IO  
+import Control.Monad
+
+-- Defining Types
+data Command = Noop | Addx Int
+    deriving (Show, Read, Eq)
+
+type His = [(Int, Int)]
+type CPU = (Int, Int, His)
+
+-- Getting Input
+translateInput :: String -> [Command]
+translateInput [] = []
+translateInput str = [getCommand line | line <- lines str]
+    where
+        getCommand :: String -> Command
+        getCommand "noop" = Noop
+        getCommand ('a':'d':'d':'x':' ':x) = Addx (read x)
+        getCommand _ = error "Invalid Command [Error 1]"
+
+-- Defining helper functions
+runCommand :: Command -> CPU -> CPU
+runCommand Noop (x, c, his) = (x, c + 1, ((x, c + 1):his))
+runCommand (Addx v) (x, c, his) = (x + v, c + 2, ((x, c + 2):(x, c + 1):his))
+
+runCommands :: [Command] -> CPU -> CPU
+runCommands (cmd:cmds) cpu = runCommands cmds (runCommand cmd cpu)
+runCommands [] cpu = cpu
+
+getHistory :: CPU -> His
+getHistory (_, _, his) = reverse his
+
+getScore :: His -> Int
+getScore [] = 0
+getScore ((x, c):his) | (c - 20) `mod` 40 == 0 = (x * c) + (getScore his)
+                      | otherwise = getScore his
+
+-- Part1
+part1 :: [Command] -> Int
+part1 cmds = getScore $ getHistory $ runCommands cmds (1, 0, [])
+
+main = do
+        handle <- openFile "d10/d10.txt" ReadMode
+        contents <- hGetContents handle
+        print $ part1 $ translateInput contents
+        hClose handle
---
+++ d10/d10p2.hs
@@ -0,0 +1,52 @@
+
+import System.IO  
+import Control.Monad
+
+-- Defining Types
+data Command = Noop | Addx Int
+    deriving (Show, Read, Eq)
+
+type His = [(Int, Int)]
+type CPU = (Int, Int, His)
+
+-- Getting Input
+translateInput :: String -> [Command]
+translateInput [] = []
+translateInput str = [getCommand line | line <- lines str]
+    where
+        getCommand :: String -> Command
+        getCommand "noop" = Noop
+        getCommand ('a':'d':'d':'x':' ':x) = Addx (read x)
+        getCommand _ = error "Invalid Command [Error 1]"
+
+-- Defining helper functions
+runCommand :: Command -> CPU -> CPU
+runCommand Noop (x, c, his) = (x, c + 1, ((x, c + 1):his))
+runCommand (Addx v) (x, c, his) = (x + v, c + 2, ((x, c + 2):(x, c + 1):his))
+
+runCommands :: [Command] -> CPU -> CPU
+runCommands (cmd:cmds) cpu = runCommands cmds (runCommand cmd cpu)
+runCommands [] cpu = cpu
+
+getHistory :: CPU -> His
+getHistory (_, _, his) = reverse his
+
+absDiff :: Num a => a -> a -> a
+absDiff n = abs . (n-)  
+
+getScreening :: His -> String
+getScreening ((x, c):his) | absDiff x ((c `mod` 40) - 1) <= 1 && c `mod` 40 == 0 = '#':'\n':(getScreening his)
+                          | absDiff x ((c `mod` 40) - 1) <= 1 = '#':(getScreening his)
+                          | c `mod` 40 == 0 = '.':'\n':(getScreening his)
+                          | otherwise = '.':(getScreening his)
+getScreening [] = ""
+
+-- Part2
+part2 :: [Command] -> String
+part2 cmds = getScreening $ getHistory $ runCommands cmds (1, 0, [])
+
+main = do
+        handle <- openFile "d10/d10.txt" ReadMode -- I've left this input as my input because it looks cool
+        contents <- hGetContents handle
+        putStr $ part2 $ translateInput contents
+        hClose handle
